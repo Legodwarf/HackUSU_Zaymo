96bf11ca327f9b45a1d5211a77e411e18709329f
Wesley Thompson <a02397152@usu.edu>
2026-02-27 23:11:03 -0700

    Update CallJSearch.py

--- CallJSearch.py
+++ CallJSearch.py
@@ -1,1 +1,19 @@
 
+import requests
+
+url = "https://api.apilayer.com/jsearch/search"
+
+params = {
+    "query": "data engineer",
+    "page": "1",
+    "num_pages": "1",
+    "date_posted": "all"
+}
+
+headers = {
+    "apikey": "YOUR_API_KEY"
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+print(response.json())
